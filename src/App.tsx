1e09be8644548ceab84f8cbf8657e7dd42a5eaae
vishnuxx <vishnuvs2002it@gmail.com>
2023-07-20 15:19:32 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import type { Component } from 'solid-js';
+
+import styles from './App.module.css';
+
+import SplitX, { SCol } from './lib/SplitX';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      
+    </div>
+  );
+};
+
+export default App;
